dfd07ba73d3e9b6173fea090c9ce5d9ad3d1b754
Rainy <1948118433@qq.com>
2018-12-26 21:59:30 +0800

    User profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    /*
+     * 个人信息展示页
+     */
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,10 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+//用户模块
+Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
+
+//以上代码等同于以下代码
+//Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+//Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+//Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
