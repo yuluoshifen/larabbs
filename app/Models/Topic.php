bb3c5646b23bf63d9678488ac4a972cd456bdce5
Rainy <1948118433@qq.com>
2019-01-05 15:18:51 +0800

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,20 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    /**
+     * 话题和分类一对一关系
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * 话题和用户一对一关系
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
